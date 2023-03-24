c91cade26a04adefcd1e68dd7fc059c942f022f9
brayan rodriguez <brayanrodriguez@gmail.com>
2023-03-24 08:23:56 -0500

    se añadio el archivo indes.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form name="formulario" method ="post">
+ingrese un numero
+<input type="numero" name="n">
+<input type="submit" name="mostar" value ="mostrar">
+
+
+</form>
+
+<?php
+$mostrar=$_POST['n'];
+
+echo $mostrar;
+?>
+
+
+</body>
+</html>
